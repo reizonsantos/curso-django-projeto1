c7c06a0474960b1f9749780f1b1d55215ffe1fc0
Reizon Santos <reizonsantos96@gmail.com>
2022-06-22 20:52:34 -0300

    Adicionado head

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,9 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-
-def contato(request):
-    return HttpResponse('Contato')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Reizon',
+    })
